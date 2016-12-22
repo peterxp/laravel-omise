972b009de677b64926b0479b67a9fdde9c25149a
supachai <peterxp@hotmail.com>
2016-12-22 14:39:03 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,3 +1,3 @@
 <?php
-define('OMISE_PUBLIC_KEY', 'pkey_test_51z4rr9fpvp546yfysp');
-define('OMISE_SECRET_KEY', 'skey_test_51z5fp9zfnev8d3dqww');
+define('OMISE_PUBLIC_KEY', 'pkey_test_51zz4rr9fpvp546yfysp');
+define('OMISE_SECRET_KEY', 'skey_test_51zz5fp9zfnev8d3dqww');
